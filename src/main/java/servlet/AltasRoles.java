ac2a9f4ea85be660bab9f00dc5805c449798b5b4
Sergio <alonsodecaso.sergio@gmail.com>
2021-04-29 17:45:16 +0200

    Funciona la inserccion de roles

--- src/main/java/servlet/AltasRoles.java
+++ src/main/java/servlet/AltasRoles.java
@@ -1,11 +1,23 @@
 package servlet;
 
 import java.io.IOException;
+
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+import dataModelDAO.RolesDAO;
+import dataModelEntities.Roles;
+import dataModelUtils.HibernateUtil;
 
 /**
  * Servlet implementation class AltasRoles
@@ -13,6 +25,11 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/AltasRoles")
 public class AltasRoles extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+    
+	private static Logger logger = LogManager.getLogger(Loggin.class);
+	
+	static SessionFactory sessionFactory;
+	static Session session;
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -21,13 +38,32 @@ public class AltasRoles extends HttpServlet {
         super();
         // TODO Auto-generated constructor stub
     }
+    
+    public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+		
+		//INFORMACION DE INICIO DEL PROGRAMA Y INICIO DE LA SESSION DE HIBERNATE
+		logger.info("%1$s: >>>>>> Main execution started. MOSTRAR DATOS");
+		
+		session = HibernateUtil.getSessionFactory().openSession();
+	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		String nombre = request.getParameter("nombre");
+		
+		Roles rol = new Roles(nombre);
+		
+		Transaction tx = session.beginTransaction();
+		
+		RolesDAO.insertRol(session, rol);
+		
+		tx.commit();
+		
 	}
 
 	/**
